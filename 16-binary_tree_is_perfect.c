42789c663b37bb31bc487532d4992eab9fdd8893
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-08-08 14:04:39 +0200

    chore: Refactor binary_tree_is_perfect function for improved efficiency

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -14,33 +14,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
+
 	if (tree->left != NULL)
 		left_hgt = binary_tree_height(tree->left) + 1;
 	if (tree->right != NULL)
 		right_hgt = binary_tree_height(tree->right) + 1;
+
 	return (left_hgt >= right_hgt ? left_hgt : right_hgt);
-}
-
-/**
- * binary_tree_balance - Write a function that measures
- * the balance factor of a binary tree.
- * @tree: is a ptr to the root node of the tree to measure
- * the balance factor.
- *
- * Return: the balance factor of the tree or 0.
- */
-int binary_tree_balance(const binary_tree_t *tree)
-{
-	int left_hgt = 0, right_hgt = 0;
-
-	if (tree == NULL)
-		return (0);
-	if (tree->left != NULL)
-		left_hgt = binary_tree_height(tree->left) + 1;
-	if (tree->right != NULL)
-		right_hgt = binary_tree_height(tree->right) + 1;
-
-	return (left_hgt - right_hgt);
 }
 
 /**
@@ -50,15 +30,15 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int left_height, right_height;
+
 	if (tree == NULL)
 		return (0);
 
-	if (binary_tree_balance(tree) == 0)
-	{
-		if (binary_tree_is_perfect(tree->left) == 0 &&
-			binary_tree_is_perfect(tree->right) == 0)
-			return (1);
-	}
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	if (left_height == right_height)
+		return (1);
+
 	return (0);
 }
-/*ne marche pas dans le cas d'un arbre simple !!!*/
